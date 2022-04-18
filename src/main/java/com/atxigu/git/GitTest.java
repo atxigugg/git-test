1863fa315c0327d5e9765edd47446605554b4f8b
Gugang <Gugang@163.com>
2022-04-18 21:46:02 +0800

    first commit

---
+++ src/main/java/com/atxigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atxigu.git;
+
+/**
+ * @author ggstart
+ * @create 2022-04-18-21:40
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+}
